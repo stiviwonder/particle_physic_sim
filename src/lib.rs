5ba31027964e2eba2d1abd6a0277dd3acc76e9e1
stiviwonder <sevicar3@gmail.com>
2022-10-04 14:01:34 +0200

    camera reposition

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ pub use particle::ParticlePlugin;
 pub const WIDTH: f32 = 1080.0;
 pub const HEIGHT: f32 = 600.0;
 
-pub const CAM_X: f32 = 100.;
-pub const CAM_Y: f32 = 30.;
-pub const CAM_Z: f32 = 20.;
+pub const CAM_X: f32 = 130.;
+pub const CAM_Y: f32 = 100.;
+pub const CAM_Z: f32 = 50.;
